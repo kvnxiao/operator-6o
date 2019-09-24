c677339e9d9703ee2a2447de5bde30f1600589ff
Kevin Xiao <kevin.xiao@uber.com>
2019-09-24 11:12:46 -0700

    Add validations for context and message

---
+++ src/main/kotlin/com/github/kvnxiao/discord/command/validation/context/PermissionValidator.kt
@@ -0,0 +1,30 @@
+/**
+ *   Copyright 2019 Ze Hao (Kevin) Xiao
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.github.kvnxiao.discord.command.validation.context
+
+import com.github.kvnxiao.discord.command.context.Context
+import com.github.kvnxiao.discord.command.validation.Validator
+import discord4j.core.`object`.entity.TextChannel
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+
+object PermissionValidator : Validator<Context> {
+    override fun validate(value: Context): Mono<Boolean> =
+        if (value.isDirectMessage) true.toMono()
+        else (value.channel as TextChannel)
+            .getEffectivePermissions(value.event.member.get().id)
+            .map { it.containsAll(value.permissions.permSet) }
+}
---
+++ src/main/kotlin/com/github/kvnxiao/discord/command/validation/message/SourceValidator.kt
@@ -0,0 +1,32 @@
+/**
+ *   Copyright 2019 Ze Hao (Kevin) Xiao
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.github.kvnxiao.discord.command.validation.message
+
+import com.github.kvnxiao.discord.command.validation.Validator
+import discord4j.core.`object`.entity.Message
+import reactor.core.publisher.Mono
+
+/**
+ * Checks if the message is non-empty, has an author, and was sent by a non-bot user.
+ */
+object SourceValidator : Validator<Message> {
+    override fun validate(value: Message): Mono<Boolean> =
+        Mono.just(
+            value.content.map(String::isNotBlank).orElse(false) &&
+                    value.author.isPresent &&
+                    value.author.map { !it.isBot }.orElse(false)
+        )
+}
