a3ff159b155ad69cf3b688a992220a96d842348c
Kevin Xiao <kevin.xiao@uber.com>
2019-10-18 13:55:43 -0700

    Refactor XKCD command with post number specifier

--- src/main/kotlin/com/github/kvnxiao/discord/commands/search/XkcdCommand.kt
+++ src/main/kotlin/com/github/kvnxiao/discord/commands/search/XkcdCommand.kt
@@ -15,13 +15,16 @@
  */
 package com.github.kvnxiao.discord.commands.search
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.kvnxiao.discord.command.annotation.Alias
 import com.github.kvnxiao.discord.command.annotation.Descriptor
 import com.github.kvnxiao.discord.command.annotation.Id
+import com.github.kvnxiao.discord.command.annotation.SubCommand
 import com.github.kvnxiao.discord.command.context.Context
 import com.github.kvnxiao.discord.command.executable.Command
-import com.github.kvnxiao.discord.http.HttpRequest
 import com.github.kvnxiao.discord.http.HttpResponseHandler
+import com.github.kvnxiao.discord.http.isSuccessCode
 import discord4j.core.`object`.entity.Message
 import java.awt.Color
 import java.util.concurrent.ThreadLocalRandom
@@ -32,50 +35,38 @@ import reactor.netty.ByteBufMono
 import reactor.netty.http.client.HttpClient
 import reactor.netty.http.client.HttpClientResponse
 
-@Component
-@Id("xkcd")
-@Descriptor(
-    description = "Displays a random post from XKCD, or the latest post from XKCD with `latest`.",
-    usage = "%A | %A `latest`"
+private data class XKCDResponse(
+    val month: String,
+    val num: Int,
+    val link: String,
+    val year: String,
+    val news: String,
+    val safe_title: String,
+    val transcript: String,
+    val alt: String,
+    val img: String,
+    val title: String,
+    val day: String
 )
-class XkcdCommand : Command, HttpResponseHandler {
-    companion object {
-        private val XKCD_API_URL_BUILDER: URIBuilder
-            get() = URIBuilder()
-                .setScheme("https")
-                .setHost("xkcd.com")
-        private const val DEFAULT_PATH = "info.0.json"
-        private val EMBED_COLOR = Color(150, 168, 200)
-        private fun client(path: String = DEFAULT_PATH): HttpClient.ResponseReceiver<*> =
-            HttpClient.create()
-                .get()
-                .uri(XKCD_API_URL_BUILDER.setPath(path).build().toASCIIString())
-    }
 
-    data class XKCDResponse(
-        val month: String,
-        val num: Int,
-        val link: String,
-        val year: String,
-        val news: String,
-        val safe_title: String,
-        val transcript: String,
-        val alt: String,
-        val img: String,
-        val title: String,
-        val day: String
-    )
+private object Xkcd {
+    val XKCD_API_URL_BUILDER: URIBuilder
+        get() = URIBuilder()
+            .setScheme("https")
+            .setHost("xkcd.com")
 
-    override fun execute(ctx: Context): Mono<Void> =
-        Mono.justOrEmpty(ctx.args.arguments)
-            .filter { it == "latest" }
-            .flatMap { getLatestPost(ctx) }
-            .switchIfEmpty(getRandomPost(ctx))
-            .then()
+    const val DEFAULT_PATH = "info.0.json"
 
-    override fun handleInputStream(ctx: Context, body: ByteBufMono): Mono<Message> =
+    val EMBED_COLOR = Color(150, 168, 200)
+
+    fun client(path: String = DEFAULT_PATH): HttpClient.ResponseReceiver<*> =
+        HttpClient.create()
+            .get()
+            .uri(XKCD_API_URL_BUILDER.setPath(path).build().toASCIIString())
+
+    fun handleInputStream(ctx: Context, body: ByteBufMono, objectMapper: ObjectMapper): Mono<Message> =
         body.asInputStream()
-            .map { HttpRequest.OBJECT_MAPPER.readValue<XKCDResponse>(it) }
+            .map { objectMapper.readValue<XKCDResponse>(it) }
             .flatMap { response ->
                 ctx.channel.createMessage { spec ->
                     spec.setEmbed { embedSpec ->
@@ -92,29 +83,84 @@ class XkcdCommand : Command, HttpResponseHandler {
                 }
             }
 
-    override fun handleError(ctx: Context, response: HttpClientResponse): Mono<Message> =
+    fun handleError(ctx: Context, response: HttpClientResponse): Mono<Message> =
         ctx.channel.createMessage(
             "Unable to query XKCD:\n${response.status().code()} - ${response.status().reasonPhrase()}"
         )
+}
+
+@Component
+@Id("xkcd")
+@Descriptor(
+    description = "Displays a random post from XKCD, or the latest post from XKCD with `latest`.",
+    usage = "%A | %A <post number> | %A `latest`"
+)
+@SubCommand([XkcdLatestCommand::class])
+class XkcdCommand(
+    private val objectMapper: ObjectMapper
+) : Command, HttpResponseHandler {
+    override fun execute(ctx: Context): Mono<Void> =
+        Mono.justOrEmpty(ctx.args.arguments)
+            .map { it.toInt() }
+            .filter { it > 0 }
+            .flatMap { getPost(ctx, it) }
+            .switchIfEmpty(getRandomPost(ctx))
+            .then()
+            .onErrorResume { Mono.empty() }
+
+    override fun handleInputStream(ctx: Context, body: ByteBufMono): Mono<Message> =
+        Xkcd.handleInputStream(ctx, body, objectMapper)
+
+    override fun handleError(ctx: Context, response: HttpClientResponse): Mono<Message> =
+        Xkcd.handleError(ctx, response)
 
     private fun getNumber(body: ByteBufMono): Mono<Int> =
         body.asInputStream()
-            .map { HttpRequest.OBJECT_MAPPER.readValue<XKCDResponse>(it) }
+            .map { objectMapper.readValue<XKCDResponse>(it) }
             .map { response -> response.num }
 
-    private fun getLatestPost(ctx: Context): Mono<Message> =
-        client().responseSingle { response, body -> handleResponse(ctx, response, body) }
+    private fun getPost(ctx: Context, postNumber: Number): Mono<Message> =
+        Xkcd.client("$postNumber/${Xkcd.DEFAULT_PATH}").responseSingle { response, body ->
+            handleResponse(
+                ctx,
+                response,
+                body
+            )
+        }
 
     private fun getRandomPost(ctx: Context): Mono<Message> =
-        client().responseSingle { response, body ->
-            if (response.status().code() in 200..299) {
+        Xkcd.client().responseSingle { response, body ->
+            if (response.isSuccessCode()) {
                 getNumber(body)
-                    .flatMap { latestNum ->
-                        client("${ThreadLocalRandom.current().nextInt(1, latestNum + 1)}/$DEFAULT_PATH")
-                            .responseSingle { response, body -> handleResponse(ctx, response, body) }
-                    }
             } else {
-                handleError(ctx, response)
+                Mono.empty()
             }
+        }.flatMap { latestNum ->
+            getPost(ctx, ThreadLocalRandom.current().nextInt(1, latestNum + 1))
         }
+}
+
+@Component
+@Id("xkcd.latest")
+@Alias(["latest"])
+@Descriptor(
+    description = "Displays the latest post from XKCD with `latest`.",
+    usage = "%A"
+)
+class XkcdLatestCommand(
+    private val objectMapper: ObjectMapper
+) : Command, HttpResponseHandler {
+    override fun execute(ctx: Context): Mono<Void> =
+        getLatestPost(ctx)
+            .then()
+            .onErrorResume { Mono.empty() }
+
+    private fun getLatestPost(ctx: Context): Mono<Message> =
+        Xkcd.client().responseSingle { response, body -> handleResponse(ctx, response, body) }
+
+    override fun handleInputStream(ctx: Context, body: ByteBufMono): Mono<Message> =
+        Xkcd.handleInputStream(ctx, body, objectMapper)
+
+    override fun handleError(ctx: Context, response: HttpClientResponse): Mono<Message> =
+        Xkcd.handleError(ctx, response)
 }
