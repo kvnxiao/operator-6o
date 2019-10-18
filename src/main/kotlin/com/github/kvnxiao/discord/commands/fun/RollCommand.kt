4ca00259de89f2160bfa4f4c587a6aa62cfb0f56
Kevin Xiao <kevin.xiao@uber.com>
2019-10-18 13:05:01 -0700

    Add number roll command

--- src/main/kotlin/com/github/kvnxiao/discord/commands/fun/RollCommand.kt
+++ src/main/kotlin/com/github/kvnxiao/discord/commands/fun/RollCommand.kt
@@ -1,3 +1,18 @@
+/**
+ *   Copyright 2019 Ze Hao (Kevin) Xiao
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.kvnxiao.discord.commands.`fun`
 
 import com.github.kvnxiao.discord.command.annotation.Descriptor
@@ -5,9 +20,9 @@ import com.github.kvnxiao.discord.command.annotation.Id
 import com.github.kvnxiao.discord.command.annotation.Permissions
 import com.github.kvnxiao.discord.command.context.Context
 import com.github.kvnxiao.discord.command.executable.Command
+import java.util.concurrent.ThreadLocalRandom
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import java.util.concurrent.ThreadLocalRandom
 
 @Component
 @Id("roll")
